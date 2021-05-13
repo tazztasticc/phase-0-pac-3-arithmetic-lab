8c67abb2bb22cc36dda8bd676a9a3369cf153ffd
Tasif <taz181995@gmail.com>
2021-05-13 17:41:30 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function increment(a){
+    return a+1;
+}
+
+function decrement(a){
+    return a-1;
+}
+
+function makeInt(String){
+    return parseInt(String,10);
+}
+
+function preserveDecimal(String){
+    return parseFloat(String);
+}
